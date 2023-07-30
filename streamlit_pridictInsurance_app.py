8a2435ba26e2491353f6713ec6e83eb706ac1dec
singh-chandanbir <110817661+singh-chandanbir@users.noreply.github.com>
2023-07-30 23:40:44 +0530

    removes extra libs

--- streamlit_pridictInsurance_app.py
+++ streamlit_pridictInsurance_app.py
@@ -1,7 +1,5 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn import metrics
